e6eb5553d7d0541af2ccbe261fe9a6df9c61283f
David L. Inman <davidlevere@gmail.com>
2022-10-31 23:28:13 -0400

    clean out test example

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,6 @@ use proc_macro::{TokenStream, TokenTree};
 
 use crate::parsing::parse;
 
-#[proc_macro]
-pub fn ikky(x : TokenStream) -> TokenStream {
-    let y = x.into_iter().collect::<Vec<TokenTree>>().iter();
-
-/*    pred!(comma<'a>: &'a TokenTree => char = |z| match z { TokenTree::Punct(p) => p.as_char() == ',', _ => false } => {
-        match z {
-            TokenTree::Punct(p) => p.as_char(),
-            _ => unreachable!(),
-        }
-    } );*/
-    //seq!(main<'a>: )
-
-    "".parse().unwrap()
-}
 
 #[proc_macro]
 pub fn blarg(x : TokenStream) -> TokenStream {
