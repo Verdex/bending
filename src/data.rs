6dafe23d9bff03259ac56e722bad606213c6f98b
David L. Inman <davidlevere@gmail.com>
2022-10-31 23:51:12 -0400

    notes

--- src/data.rs
+++ src/data.rs
@@ -11,6 +11,37 @@ pub enum Pattern {
     At(String, Box<Pattern>),*/
 }
 
+/*
+
+match _ {
+    1 => 
+    1 | 2 => 
+    1..2 =>
+    1..=2 =>
+    a =>
+    a @ _ =>
+    Blah =>
+    Blah(P) =>
+    Blah(P, ..) => ??
+    Blah { a : P } => 
+    Blah { a : P, .. } => 
+    [] =>
+    [P, P] => 
+    [P, ..] =>
+    [P, a @ ..] =>
+    A::B => (and etc)
+    () => ??
+    (a, b) =>
+    (a, .., b) =>
+    P if pred =>
+    & name =>
+    ! =>
+    ! & name =>
+}
+
+
+*/
+
 // ranges
 // .. in tuples, structs, list
 // |
