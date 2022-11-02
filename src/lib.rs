8b7654d174af08c6760e039518a6d6e5dddc4da1
David L. Inman <davidlevere@gmail.com>
2022-11-01 23:46:57 -0400

    move out the transform to iter<&tree> so that the lifetime will last longer than the function

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,9 @@ use crate::parsing::parse;
 #[proc_macro]
 pub fn blarg(input : TokenStream) -> TokenStream {
 
+    let input = input.into_iter().collect::<Vec<_>>();
+    let input = input.iter();
     let _x = parse(input).unwrap();
-
 
     "".parse().unwrap()
 }
