d6c21c1f5ea18492de0f6d468ec375b78fa55d5c
David L. Inman <davidlevere@gmail.com>
2022-11-01 23:47:20 -0400

    inc prog + lifetime param for function for the lifetime param for pattern

--- src/parsing.rs
+++ src/parsing.rs
@@ -5,19 +5,29 @@ use motif::*;
 
 use crate::data::*;
 
-pub fn parse( input : TokenStream ) -> Result<Pattern, MatchError>  {
-    let input = input.into_iter().collect::<Vec<_>>();
-    let mut input = input.iter().enumerate();
+pub fn parse<'a>( input : (impl Iterator<Item = &'a TokenTree> + Clone) ) -> Result<Pattern<'a>, MatchError>  {
+    let mut input = input.enumerate();
     pattern(&mut input)
 }
 
-group!(pattern<'a>: &'a TokenTree => Pattern = |input| {
+group!(pattern<'a>: &'a TokenTree => Pattern<'a> = |input| {
 
-    pred!(wild<'a>: &'a TokenTree => Pattern = |_x| match _x { TokenTree::Ident(n) => n.to_string() == "_", _ => false } => {
+    pred!(wild<'a>: &'a TokenTree => Pattern<'a> = |_x| match _x { TokenTree::Ident(n) => n.to_string() == "_", _ => false } => {
         Pattern::Wild
     } );
 
-    alt!(main<'a>: &'a TokenTree => Pattern = wild);
+    seq!(literal<'a>: &'a TokenTree => Pattern<'a> = lit <= TokenTree::Literal(_), { 
+        if let TokenTree::Literal(lit) = lit {
+            Pattern::Literal(lit)
+        }
+        else {
+            unreachable!()
+        }
+    });
+
+    alt!(main<'a>: &'a TokenTree => Pattern<'a> = wild
+                                                | literal 
+                                                );
 
 
     main(input)
