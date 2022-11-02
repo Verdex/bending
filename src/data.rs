390fe94b966f0b51f450295ea12bf98feca52b98
David L. Inman <davidlevere@gmail.com>
2022-11-01 23:46:06 -0400

    notes

--- src/data.rs
+++ src/data.rs
@@ -13,6 +13,16 @@ pub enum Pattern<'a> {
 
 /*
 
+it sounds like negative literals are punct + literal and then even sometimes group ( punct + literal )
+
+mut, ref, ref + mut are also possibilities in patterns, but I think we're always going to be operating 
+on a reference and then returning a reference.  mutability shouldn't enter into it because we're 
+going to be potentially returning the same inner parts of the data many times
+    
+    ref is about getting a ref to something that isn't, but I don't think we ever want ownership, so we'll always have ref already
+    mut is about changing it, but we want to give all possible instances of the pattern, which can include the same data more than once
+        so mutating it isn't going to be an option
+
 match _ {
     1 => 
     1 | 2 => 
